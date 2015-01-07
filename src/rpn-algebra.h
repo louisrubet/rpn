89e55944a7fd4e50909d587acdc1f2273c0e42ab
Louis <louis@rubet.fr>
2015-01-07 10:08:54 +0100

    Add general algebra commands inv, %, %CH, ^, sqrt, sq

--- src/rpn-algebra.h
+++ src/rpn-algebra.h
@@ -1,3 +1,5 @@
+//TODO please do like in rpn-trig.h:
+// ex: ((number*)_stack->get_obj(0))->_value = tan(((number*)_stack->get_obj(0))->_value);
 void plus()
 {
 	MIN_ARGUMENTS(2);
@@ -43,4 +45,63 @@ void neg()
 	ARG_IS_OF_TYPE(0, cmd_number);
 
 	putf(-getf());
+}
+
+void inv()
+{
+	MIN_ARGUMENTS(1);
+	ARG_IS_OF_TYPE(0, cmd_number);
+
+	// arithmetic faults are managed by c++
+	putf(1 / getf());
+}
+
+void purcent()
+{
+	MIN_ARGUMENTS(2);
+	ARG_IS_OF_TYPE(0, cmd_number);
+	ARG_IS_OF_TYPE(1, cmd_number);
+
+	putf((getf() * getf()) / 100);
+}
+
+void purcentCH()
+{
+	MIN_ARGUMENTS(2);
+	ARG_IS_OF_TYPE(0, cmd_number);
+	ARG_IS_OF_TYPE(1, cmd_number);
+
+	// arithmetic faults are managed by c++
+	floating_t first = getf();
+	putf((100 * first) / getf());
+}
+
+void power()
+{
+	MIN_ARGUMENTS(2);
+	ARG_IS_OF_TYPE(0, cmd_number);
+	ARG_IS_OF_TYPE(1, cmd_number);
+
+	// arithmetic faults are managed by c++
+	floating_t first = getf();
+	putf(powl(getf(), first));
+}
+
+//TODO 2 2 sqrt sq - does not zero !!
+void squareroot()
+{
+	MIN_ARGUMENTS(1);
+	ARG_IS_OF_TYPE(0, cmd_number);
+
+	// arithmetic faults are managed by c++
+	putf(sqrtl(getf()));
+}
+
+void square()
+{
+	MIN_ARGUMENTS(1);
+	ARG_IS_OF_TYPE(0, cmd_number);
+
+	floating_t first = getf();
+	putf(first * first);
 }
