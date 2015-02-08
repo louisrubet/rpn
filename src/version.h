2d3feda213b801469e3e9f96d6b1d21cb35721d2
louis <louis@rubet.fr>
2015-02-08 13:37:01 +0100

    version 1.1

--- src/version.h
+++ src/version.h
@@ -1,2 +1,2 @@
-static const char version[] = "1.0";
-static const char uname[] = "rpn v1.0, (c) 2013 <louis@rubet.fr>";
+static const char version[] = "1.1";
+static const char uname[] = "rpn v1.1, (c) 2013 <louis@rubet.fr>";
