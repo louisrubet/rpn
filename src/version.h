05bf8ed87bd4cb98eab253c998b4ae01984ad1a1
Louis <louis@rubet.fr>
2015-02-11 11:11:55 +0100

    Changed version to 1.2

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 static const char version[] = "1.1";
-static const char uname[] = "rpn v1.1, (c) 2013 <louis@rubet.fr>";
+static const char uname[] = "rpn v1.2, (c) 2015 <louis@rubet.fr>";
 static const char prompt[] = "rpn> ";
