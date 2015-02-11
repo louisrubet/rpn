369d04745864ebf77990a4fefa92f79ea0d4eb6f
Louis <louis@rubet.fr>
2015-02-11 18:19:21 +0100

    version 1.3-beta

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
-static const char version[] = "1.2";
-static const char uname[] = "rpn v1.2, (c) 2015 <louis@rubet.fr>";
+static const char version[] = "1.3-beta";
+static const char uname[] = "rpn v1.3-beta, (c) 2015 <louis@rubet.fr>";
 static const char prompt[] = "rpn> ";
