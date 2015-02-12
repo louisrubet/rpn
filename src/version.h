897c0d52335f029cc6369f06b7ebb7e941221e1a
Louis <louis@rubet.fr>
2015-02-12 11:10:00 +0100

    Added ANSI escape sequences, prompt is bold

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 static const char version[] = "1.3-beta";
 static const char uname[] = "rpn v1.3-beta, (c) 2015 <louis@rubet.fr>";
-static const char prompt[] = "rpn> ";
+static const char prompt[] = ATTR_BOLD "rpn" ATTR_OFF "> ";
