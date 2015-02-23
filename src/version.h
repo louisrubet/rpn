6f9a94b34d12dfa7e6c0f4b87048b296e6724bdc
Louis <louis@rubet.fr>
2015-02-23 17:33:05 +0100

    Rationalize syntax strings

--- src/version.h
+++ src/version.h
@@ -1,3 +1,13 @@
+// version and soft name
 static const char version[] = "1.3-beta";
 static const char uname[] = "rpn v1.3-beta, (c) 2015 <louis@rubet.fr>";
+
+// syntax
+static const char* syntax[] = {
+	"Reverse Polish Notation language, based on hewlett-Packard RPL",
+	"Syntax: rpn [command]",
+	"with optional command = list of commands",
+	NULL
+};
+
 static const char prompt[] = ATTR_BOLD "rpn" ATTR_OFF "> ";
