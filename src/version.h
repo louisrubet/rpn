b0d1522c2787b09fb09d88c3db58834eb94a1890
Louis Rubet <louis@rubet.fr>
2015-03-16 11:38:31 +0100

    v1.3.1

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // version and soft name
-static const char version[] = "1.4-beta";
-static const char uname[] = "rpn v1.4-beta, (c) 2015 <louis@rubet.fr>";
+static const char version[] = "1.3.1";
+static const char uname[] = "rpn v1.3.1, (c) 2015 <louis@rubet.fr>";
 
 static const char g_cursor[] = "> ";
 static const string g_show_stack_separator = "> ";
