25973265bac41cf69322cac2b447c5c3e04c9bf6
Louis <louis@rubet.fr>
2015-04-24 09:31:42 +0200

    Version 1.3.2

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 // version and soft name
-static const char version[] = "1.3.1";
-static const char uname[] = "rpn v1.3.1, (c) 2015 <louis@rubet.fr>";
+static const char version[] = "1.3.2";
+static const char uname[] = "rpn v1.3.2, (c) 2015 <louis@rubet.fr>";
 
 static const char g_cursor[] = "> ";
 static const string g_show_stack_separator = "> ";
