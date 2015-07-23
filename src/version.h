f11505bf42d8394c3c5bcdce238e0530d5305333
Louis Rubet <lrubet@gmail.com>
2015-07-23 12:00:54 +0200

    Added modulo fn

--- src/version.h
+++ src/version.h
@@ -1,13 +1,16 @@
 // version and soft name
-static const char version[] = "1.3.2";
-static const char uname[] = "rpn v1.3.2, (c) 2015 <louis@rubet.fr>";
+static const char version[] = "1.3.3";
+static const char uname[] = "rpn v1.3.3, (c) 2015 <louis@rubet.fr>";
 
 static const char g_cursor[] = "> ";
 static const string g_show_stack_separator = "> ";
 
 // syntax
 static const char* syntax[] = {
-    "Reverse Polish Notation language, based on hewlett-Packard RPL",
+    ATTR_BOLD "R" ATTR_OFF "everse "
+    ATTR_BOLD "P" ATTR_OFF "olish "
+    ATTR_BOLD "N" ATTR_OFF "otation language, based on hewlett-Packard RPL",
+    "",
     "Syntax: rpn [command]",
     "with optional command = list of commands",
     NULL
