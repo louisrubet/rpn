4670aa77c0d7ef00f4e32e66e9198967ef43b03d
Louis Rubet <louis@rubet.fr>
2017-04-17 23:10:53 +0200

    #34: MPFR lib, first attempt

--- src/rpn-general.h
+++ src/rpn-general.h
@@ -15,7 +15,7 @@ void verbose()
 {
     MIN_ARGUMENTS(1);
     ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    g_verbose = (int)getf();
+    g_verbose = int(getf());
 }
 
 void help()
@@ -75,7 +75,7 @@ void std()
     if (stack_size()>=1)
     {
         ARG_MUST_BE_OF_TYPE(0, cmd_number);
-        number::s_default_precision = (int)getf();
+        number::s_default_precision = int(getf());
     }
     number::s_current_precision = number::s_default_precision;
     number::s_mode = number::std;
@@ -89,7 +89,7 @@ void fix()
     MIN_ARGUMENTS(1);
     ARG_MUST_BE_OF_TYPE(0, cmd_number);
 
-    number::s_current_precision = (int)getf();
+    number::s_current_precision = int(getf());
     number::s_mode = number::fix;
 
     cout << setprecision(number::s_current_precision) << fixed;
