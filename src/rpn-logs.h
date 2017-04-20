b9d0277a62587ece7f70e2af6e5eac2e8ac308b6
Louis Rubet <louis@rubet.fr>
2017-04-20 22:46:48 +0200

    #34: implements logs, real, test, trig commands with MPFR

--- src/rpn-logs.h
+++ src/rpn-logs.h
@@ -1,105 +1,97 @@
-#define _USE_MATH_DEFINES
-
-//
 void rpn_e(void)
 {
-    //TODO
-    //putf(M_E);
+    number num;
+    CHECK_MPFR(mpfr_const_euler(&num._value.mpfr, s_mpfr_rnd));
+    _stack->push_back(&num, num.size(), cmd_number);
+    ((number*)_stack->back())->ensure_significand();
 }
 
 void rpn_log()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = log10(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_log10(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_alog()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = pow(((number*)_stack->get_obj(0))->_value, 10);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_exp10(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_ln()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = log(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_log(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_exp()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = exp(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_exp(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_sinh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = sinh(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_sinh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_asinh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
 
-    //floating_t value = ((number*)_stack->get_obj(0))->_value;
-    //if(value>0)
-    //    value = log(value + sqrt(value * value + 1));
-    //else
-    //    value = -log(-value + sqrt(value * value + 1));
-
-    //((number*)_stack->get_obj(0))->_value = value;
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_asinh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_cosh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = cosh(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_cosh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_acosh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
 
-    //floating_t value = ((number*)_stack->get_obj(0))->_value;
-    //if(value>0)
-    //    value = log(value + sqrt(value * value - 1));
-    //else
-    //    value = -log(-value + sqrt(value * value - 1));
-
-    //((number*)_stack->get_obj(0))->_value = value;
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_cosh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_tanh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
-    //((number*)_stack->get_obj(0))->_value = tanh(((number*)_stack->get_obj(0))->_value);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_tanh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
 
 void rpn_atanh()
 {
-    //TODO
-    //MIN_ARGUMENTS(1);
-    //ARG_MUST_BE_OF_TYPE(0, cmd_number);
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
 
-    //floating_t value = ((number*)_stack->get_obj(0))->_value;
-    //value = (log(1 + value) - log(1 - value)) / 2;
-    //((number*)_stack->get_obj(0))->_value = value;
+    floating_t* left = &((number*)_stack->get_obj(0))->_value;
+    CHECK_MPFR(mpfr_atanh(&left->mpfr, &left->mpfr, s_mpfr_rnd));
 }
