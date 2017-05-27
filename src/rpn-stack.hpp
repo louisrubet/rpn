7d48a25890425c0b0c3eb95d691ee423ae4f621d
Louis Rubet <louis@rubet.fr>
2017-05-27 14:08:09 +0200

    #77: added roll, rolld, over, dupn, dropn

--- src/rpn-stack.hpp
+++ src/rpn-stack.hpp
@@ -8,8 +8,7 @@ void swap(void)
     (void)_stack->pop_back();
     stack::copy_and_push_back(_branch_stack, 0, *_stack);
     stack::copy_and_push_back(_branch_stack, 1, *_stack);
-    (void)_branch_stack.pop_back();
-    (void)_branch_stack.pop_back();
+    _branch_stack.erase();
 }
 
 void drop(void)
@@ -23,6 +22,18 @@ void drop2(void)
     MIN_ARGUMENTS(2);
     (void)_stack->pop_back();
     (void)_stack->pop_back();
+}
+
+void dropn(void)
+{
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    int args = (int)mpfr_get_si(((number*)_stack->back())->_value.mpfr, floating_t::s_mpfr_rnd);
+    MIN_ARGUMENTS(args+1);
+
+    for(int i=0;i<args+1;i++)
+        (void)_stack->pop_back();
 }
 
 void erase(void)
@@ -42,6 +53,19 @@ void dup2(void)
     MIN_ARGUMENTS(2);
     stack::copy_and_push_back(*_stack, _stack->size()-2, *_stack);
     stack::copy_and_push_back(*_stack, _stack->size()-2, *_stack);
+}
+
+void dupn(void)
+{
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    int args = (int)mpfr_get_si(((number*)_stack->back())->_value.mpfr, floating_t::s_mpfr_rnd);
+    MIN_ARGUMENTS(args+1);
+    _stack->pop_back();
+    
+    for (int i=0;i<args;i++)
+        stack::copy_and_push_back(*_stack, _stack->size()-args, *_stack);
 }
 
 void pick(void)
@@ -74,9 +98,7 @@ void rot(void)
     stack::copy_and_push_back(_branch_stack, 1, *_stack);
     stack::copy_and_push_back(_branch_stack, 2, *_stack);
     stack::copy_and_push_back(_branch_stack, 0, *_stack);
-    (void)_branch_stack.pop_back();
-    (void)_branch_stack.pop_back();
-    (void)_branch_stack.pop_back();
+    _branch_stack.erase();
 }
 
 void depth(void)
@@ -84,4 +106,55 @@ void depth(void)
     unsigned long depth = (unsigned long)_stack->size();
     number* num = (number*)_stack->allocate_back(number::calc_size(), cmd_number);
     num->set(depth);
+}
+
+void roll(void)
+{
+    MIN_ARGUMENTS(1);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    int args = (int)mpfr_get_si(((number*)_stack->back())->_value.mpfr, floating_t::s_mpfr_rnd);
+    MIN_ARGUMENTS(args+1);
+    _stack->pop_back();
+
+    for(int i=0;i<args;i++)
+    {
+        stack::copy_and_push_back(*_stack, _stack->size()-1, _branch_stack);
+        (void)_stack->pop_back();
+    }
+
+    for(int i=1;i<args;i++)
+        stack::copy_and_push_back(_branch_stack, args-1-i, *_stack);
+    stack::copy_and_push_back(_branch_stack, args-1, *_stack);
+
+    _branch_stack.erase();
+}
+
+void rolld(void)
+{
+    MIN_ARGUMENTS(2);
+    ARG_MUST_BE_OF_TYPE(0, cmd_number);
+
+    int args = (int)mpfr_get_si(((number*)_stack->back())->_value.mpfr, floating_t::s_mpfr_rnd);
+    MIN_ARGUMENTS(args+1);
+    _stack->pop_back();
+
+    for(int i=0;i<args;i++)
+    {
+        stack::copy_and_push_back(*_stack, _stack->size()-1, _branch_stack);
+        (void)_stack->pop_back();
+    }
+
+    stack::copy_and_push_back(_branch_stack, 0, *_stack);
+    for(int i=1;i<args;i++)
+        stack::copy_and_push_back(_branch_stack, args-i, *_stack);
+
+    _branch_stack.erase();
+}
+
+void over(void)
+{
+    MIN_ARGUMENTS(2);
+
+    stack::copy_and_push_back(*_stack, _stack->size()-2, *_stack);
 }
