54a706a1d9b6c9c0a012a83d53846e8a2a74dd61
Louis Rubet <louis@rubet.fr>
2017-06-10 18:41:35 +0200

    #96: v2.1

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 // version and soft name
-static const char version[] = "2.1-RC1";
+static const char version[] = "2.1";
 static const char uname[] =
-    "rpn v2.0, (c) 2017 <louis@rubet.fr>, GNU LGPL v3";
+    "rpn v2.1, (c) 2017 <louis@rubet.fr>, GNU LGPL v3";
 
 // description
 static const char description[] =
