9ddeb3dc74831452249178ed8fd2babd8ef43c4d
Louis Rubet <louis@rubet.fr>
2017-06-12 22:34:46 +0200

    #120: version management

--- src/version.h
+++ src/version.h
@@ -1,18 +1,20 @@
 // version and soft name
-static const char version[] = "2.1";
+#define RPN_VERSION "2.1"
+static const char version[] = RPN_VERSION;
 static const char uname[] =
-    "rpn v2.1, (c) 2017 <louis@rubet.fr>, GNU LGPL v3";
+    "rpn v" RPN_VERSION ", (c) 2017 <louis@rubet.fr>, GNU LGPL v3\n";
+
+#define STRINGIFY(a) STRINGIFY_ONE(a)
+#define STRINGIFY_ONE(a) #a
 
 // description
 static const char description[] =
     ATTR_BOLD "R" ATTR_OFF "everse "
     ATTR_BOLD "P" ATTR_OFF "olish "
-    ATTR_BOLD "N" ATTR_OFF "otation language, based on "
-    "Hewlett-Packard RPL language\n\n"
-    "using " ATTR_BOLD "MPFR library" ATTR_OFF " v" MPFR_VERSION_STRING " under GNU LGPL v3\n"
-    "for multiple-precision floating-point computations with correct rounding\n\n"
-    "and " ATTR_BOLD "linenoise-ng" ATTR_OFF " v" LINENOISE_VERSION " under BSD license\n"
-    "for line editing, autocompletion and history\n";
+    ATTR_BOLD "N" ATTR_OFF "otation language\n\n"
+    "using " ATTR_BOLD "GMP" ATTR_OFF " v" STRINGIFY(__GNU_MP_VERSION) "." STRINGIFY(__GNU_MP_VERSION_MINOR) "." STRINGIFY(__GNU_MP_VERSION_PATCHLEVEL) " under GNU LGPL\n"
+    ATTR_BOLD "MPFR" ATTR_OFF " v" MPFR_VERSION_STRING " under GNU LGPL\n"
+    "and " ATTR_BOLD "linenoise-ng" ATTR_OFF " v" LINENOISE_VERSION " under BSD\n";
     
 // syntax
 static const char syntax[] =
