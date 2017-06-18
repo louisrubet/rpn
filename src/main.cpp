855f99ed2e2a530de5d835216b2e0db1be46ee79
Louis Rubet <louis@rubet.fr>
2017-06-18 14:40:08 +0200

    #125, #152: caught CtrlC during prog execution, caught SIGSEGV

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,53 @@
+// std c
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+
+// internal includes 
 #include "program.hpp"
 
 static heap s_global_heap;
 static stack s_global_stack;
+static program* s_prog_to_interrupt = NULL;
+
+// handle CtrlC signal
+static void ctrlc_handler(int sig, siginfo_t* siginfo, void* context)
+{
+    if (s_prog_to_interrupt != NULL)
+    {
+        s_prog_to_interrupt->stop();
+        s_prog_to_interrupt = NULL;
+    }
+}
+
+// handle SIGSEGV signal
+static void segv_handler(int sig, siginfo_t* siginfo, void* context)
+{
+    fprintf(stderr, "Internal error\n");
+}
+
+static void catch_signals(program* prog)
+{
+    struct sigaction act;
+
+    s_prog_to_interrupt = prog;
+
+    act.sa_sigaction = &ctrlc_handler;
+    act.sa_flags = SA_SIGINFO;
+    if (sigaction(SIGINT, &act, NULL) < 0)
+        (void)fprintf(stderr, "Warning, Ctrl-C cannot be catched [errno=%d '%s']", errno, strerror(errno));
+
+    act.sa_sigaction = &segv_handler;
+    act.sa_flags = SA_SIGINFO;
+    if (sigaction(SIGSEGV, &act, NULL) < 0)
+        (void)fprintf(stderr, "Warning, SIGSEGV cannot be catched [errno=%d '%s']", errno, strerror(errno));
+}
 
 //
 int main(int argc, char* argv[])
 {
     int ret = 0;
-
+    
     // apply default configuration
     program::apply_default();
     
@@ -23,6 +63,9 @@ int main(int argc, char* argv[])
                 break;
             else
             {
+                // user could stop prog with CtrlC
+                catch_signals(&prog);
+
                 // run it
                 if (prog.run(s_global_stack, s_global_heap) == ret_good_bye)
                     break;
@@ -50,6 +93,9 @@ int main(int argc, char* argv[])
         if (ret == ret_ok)
         {
             string separator = "";
+
+            // user could stop prog with CtrlC
+            catch_signals(&prog);
 
             // run it
             ret = prog.run(s_global_stack, s_global_heap);
