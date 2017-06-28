5221fc195f613363111b5e5cff0d5cab1e62063e
Louis Rubet <louis@rubet.fr>
2017-06-28 23:14:01 +0200

    #158: V2.3 (generation, manual, version.h)

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 // version and soft name
-#define RPN_VERSION "2.2"
+#define RPN_VERSION "2.3"
 static const char version[] = RPN_VERSION;
 static const char uname[] =
     "rpn v" RPN_VERSION ", (c) 2017 <louis@rubet.fr>, GNU LGPL v3\n";
