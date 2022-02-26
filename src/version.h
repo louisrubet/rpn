44c0cb626f88c07b8952aa200ca46121a20a7f9d
Louis Rubet <louis@rubet.fr>
2022-02-26 15:22:26 +0100

    git tag used as rpn version

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #ifndef SRC_VERSION_H_
 #define SRC_VERSION_H_
 
-#define RPN_VERSION "2.4"
+#define RPN_VERSION GIT_VERSION  // set by cmake from git tag
 #define RPN_UNAME "rpn v" RPN_VERSION ", (c) 2017 <louis@rubet.fr>, GNU LGPL v3"
 
 #endif  // SRC_VERSION_H_
